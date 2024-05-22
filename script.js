8656ccf3ba875ccbbe437e6cf889bc5bae54c288
JHENYFER VICTORIA NEVES DOS SANTOS <55406272888@info.local>
2024-05-22 10:53:57 -0300

    feat: menu terminado

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
+let menu = document.getElementById("menu")
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("icone-x")
 
+function abreFechaMenu() {
+    // Menu fechado - tem a classe menu-fechado
+    // Menu aberto - não tem a classe menu-fechado
+
+    // Alterna a classe menu-fechado
+    // menu.classList.toggle("menu-fechado")
+
+    if (menu.classList.contains("menu-fechado")) {
+        // Abrir o menu - remover a classe menu-fechado
+        menu.classList.remove("menu-fechado")
+
+        // Esconder icone barras
+        iconeBarras.style.display = "none"
+
+        // Mostrar o icone do X
+        iconeX.style.display = "inline"
+
+    }
+
+    else {
+        // Fechar o menu - adicionar a classe menu-fechado
+        menu.classList.add("menu-fechado")
+
+        // Esconder icone do X
+        iconeX.style.display = "none"
+
+        // Mostrar o icone barras
+        iconeBarras.style.display = "inline"
+    }
+
+}
+
+onresize = () => {
+    // Abrir o menu - remover a classe menu-fechado
+    menu.classList.remove("menu-fechado")
+
+    // Esconder icone barras
+    iconeBarras.style.display = "none"
+
+    // Mostrar o icone do X
+    iconeX.style.display = "inline"
+}
+
+// carossel
+
+let banner = document.querySelector(".banner")
+
+// let slides = [0, 1, 2]
+// slide[0]--> primeiro-banner
+// slide[1]--> segundo-banner
+// slide[2]--> terceiro-banner
+
+let slides = [
+    "primeiro-banner",
+    "segundo-banner",
+    "terceiro-banner"
+]
+
+let slideAtual = 0
+
+banner.classList.add(slides[slideAtual])
+
+function mostrarProximoSlide() {
+    // Remover o slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    // Somar 1 na variavel slide atual
+    slideAtual++
+
+    // Mostrar slide de acordo com o slide atual
+    banner.classList.add(slides[slideAtual])
+}
